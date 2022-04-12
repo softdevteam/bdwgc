a690dda986305d5a53038db0655532c83af865dd
Jake Hughes <jh@jakehughes.uk>
2022-04-12 15:23:17 +0100

    Add allocation switching to CI

--- .buildbot.sh
+++ .buildbot.sh
@@ -6,3 +6,7 @@ set -e
 mkdir out && cd out
 cmake -GNinja -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF -Denable_parallel_mark=ON -Dbuild_tests=ON -Denable_gc_assertions=ON ../
 ninja && ninja test
+
+cd .. && rm -rf out && mkdir out && cd out
+cmake -GNinja -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF -Denable_allocation_switching=ON -Denable_parallel_mark=ON -Dbuild_tests=ON -Denable_gc_assertions=ON ../
+ninja && ninja test
